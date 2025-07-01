6d19b5b494c9c556db5a5a25f430768c607cd4ae
Eric Kweon <erickweon96@gmail.com>
2025-06-30 23:27:17 -0400

    added credits to the asset pack

--- frontend/src/pages/Credits.jsx
+++ frontend/src/pages/Credits.jsx
@@ -2,6 +2,17 @@ export default function Credits() {
   return (
     <div>
       <h1>Credits</h1>
+      <div>
+        <h2>Sprites and Assets</h2>
+        <ul>
+          <li>
+            Sprites and assets sourced from 
+            <a href="https://danieldiggle.itch.io/sunnyside"> Sunnyside </a>
+            by <a href="https://x.com/danieldiggle"> Daniel Diggle </a> on itch.io.
+            License listed in the asset pack link.
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
